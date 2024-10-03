6625d476a00ca95bc222b2fd3d86a1ab98e64da6
Kyle Lim <96803422+str1ker577@users.noreply.github.com>
2024-10-04 02:08:36 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -51,10 +51,57 @@ st.dataframe(df.describe())
 
 
 # Table 5 - Shipping Type by Product Type - Lim, K.
+def line_chart():
+    df['Shipping Type'] = df['Shipping Type'].replace({'Express': 'Express', 'Standard': 'Standard'})
 
+    monthly_sales = df.groupby('Shipping Type')['Product Type'].count()
 
+    fig, ax = plt.subplots(figsize=(10, 5)) 
+    ax.bar(monthly_sales.index, monthly_sales.values, color='b') 
+    ax.set_title('Total Sales by Shipping Type')
+    ax.set_xlabel('Shipping Type')
+    ax.set_ylabel('Total Sales (Count)')
+    plt.xticks(rotation=45)
+
+    st.pyplot(fig)
+
+line_chart()
+
+#Observation:
+#-The chart highlights a strong preference for Standard shipping which dwarfs in comparison to other 
+#shipping types like Expedited, Express, and Overnight, while Same Day shipping shows the lowest sales count.
 
 # Table 6 - Order Status by Product Type - Lim, K.
+def pie_chart():
+    df['Product Type'] = df['Product Type'].replace({
+        'smartphone': 'Smartphone',
+        'Smartwatch': 'Smartwatch',
+        'smartwatch': 'Smartwatch',
+        'Tablet': 'Tablet',
+        'tablet': 'Tablet',
+        'Laptop': 'Laptop',
+        'laptop': 'Laptop'
+    })
+
+    product_counts = df['Product Type'].value_counts()
+
+    fig, ax = plt.subplots(figsize=(8, 8))
+    pie_colors = ['skyblue', 'salmon', 'yellow', 'lightgreen', 'orange']
+    ax.pie(product_counts, labels=product_counts.index, autopct='%1.1f%%', startangle=140, colors=pie_colors)
+    
+    ax.set_title('Order Status by Product Type')
+    ax.axis('equal')
+
+    st.pyplot(fig)
+
+pie_chart()
+
+#Observation:
+#-This graph highlights that customers prefer smartphones, leading at 29.9%, followed by tablets at 20.5%,
+#laptops and smartwatches at 19.9% and 19.7%, and finally, headphones being the least preferred at 10.1%.
+
+#- The data showcases a high customer preference for handheld devices such as smartphones and tablets, 
+# with their accesssories, like smartwatches and headphones are less prefered.
 
 
 
