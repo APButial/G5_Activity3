c9a583d8192cbae1e75434fe06ab66eff6211889
APButial <apobutial@mymail.mapua.edu.ph>
2024-10-04 18:35:40 +0800

    Changed code arrangement and replaced wrong graphs

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ import math
 import io
 
 # Note: call [streamlit run main.py] in terminal to test
-
-st.markdown("<h1 style='color: cyan;'>Customer Purchase Behavior Analysis Data Visualization</h1>",
-            unsafe_allow_html=True)
 st.write("""
          **Group Number:** 5\n
          **Section:** BM3\n
@@ -35,8 +32,8 @@ st.dataframe(df.isna().sum())
 # Show quantities
 st.dataframe(df.describe())
 
-# Table 1 - Gender Distribution - Butial
-#Lim Evan
+# functions #############################################################################
+#Lim, E
 def consumer_gender_distribution():
     """
     Displays a pie chart showing the gender distribution of consumers
@@ -58,16 +55,10 @@ def consumer_gender_distribution():
     plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
     st.pyplot(plt)
     plt.clf()
-
-#Call the fucnction
-consumer_gender_distribution()
-
-st.write("""
+    st.write("""
             Customers are almost evenly distributed between male and female, 
             with males leading by only a small difference in percentage.
          """)
-
-# Table 2 - Age Distribution - Butial
 #Butial Aivann
 def age_distribution():
   """
@@ -84,17 +75,11 @@ def age_distribution():
   plt.title('Customer Age Distribution')
   st.pyplot(plt)
   plt.clf()
-
-age_distribution()
-
-st.write("""
+  st.write("""
             Many of the customers belong to the age range of **late teens to early twenties**,
             **late forties to early fifties**, and **late seventies**. 
             For the most part, however, customers' ages are **evenly distributed**.
          """)
-
-
-# Table 3 - Quantity Sold by Product Type - Lim, E.
 def quantity_sold_by_prodtype():
   """
   quantity sold by product type in bar chart
@@ -111,18 +96,14 @@ def quantity_sold_by_prodtype():
   plt.xlabel('Product Type')
   plt.ylabel('Quantity Sold')
   plt.title('Quantity Sold by Product Type')
-  plt.show()
-
-
-quantity_sold_by_prodtype()
-
-#Observation
-#Smartphone devices have the highest quantity sold among other electronics. Over **16,000** were sold from September 2023 to September 2024. On the other hand, **headphones** have the least quantity sold of less than **60,000**. Smartwatches, laptops, and tablets, sold were relatively equal.
-
-
-
-
-# Table 4 - Quantity Sold by Smartphone Model - Lim, E.
+  st.pyplot(plt)
+  plt.clf()
+  st.write("""
+            **Smartphone devices** have the highest quantity sold among other electronics. 
+           Over **16,000** were sold from September 2023 to September 2024. On the other hand, 
+           **headphones** have the least quantity sold of less than **60,000**. Smartwatches, laptops, 
+           and tablets, sold were relatively equal.
+           """)
 #Lim Evan
 def quantity_sold_by_smartphone_model():
     """
@@ -148,89 +129,95 @@ def quantity_sold_by_smartphone_model():
     plt.xlabel('Smartphone Models')
     plt.ylabel('Number Sold')
     plt.tight_layout()
-    plt.show()
+    st.pyplot(plt)
+    plt.clf()
+    st.write("""
+        **Smartphone devices** have the highest quantity sold among other electronics. 
+         Over **16,000** were sold from September 2023 to September 2024. On the other hand, 
+         **headphones** have the least quantity sold of less than **60,000**. Smartwatches, 
+         laptops, and tablets, sold were relatively equal.
+         """)
+#Ongtangco, Randolph Joshua
+def shipping_type_by_product_bar():
+    """
+        bar chart
+        shows shipping type distribution by product type
+    """
+    # Product Type and Shipping Type grouped seperately
+    shipping_by_product = df.groupby(['Product Type', 'Shipping Type']).size().unstack(fill_value=0)
+    shipping_by_product.plot(kind='bar', stacked=True, color=['lightblue', 'salmon', 'green', 'red', 'orange'])
 
-#Calls the function quantity_sold_by_smartphone_model()
-quantity_sold_by_smartphone_model()
-
-#Observation
-#The smartphone model SKU1004 has the highest numbers sold, while the SKU1005 being the least bought. Still, the numbers sold by SKU1001 and SMP234 are comparable to SKU1004. These three models are selling well.
-
-
-# Table 5 - Shipping Type by Product Type - Lim, K.
-def line_chart():
-    df['Shipping Type'] = df['Shipping Type'].replace({'Express': 'Express', 'Standard': 'Standard'})
-
-    monthly_sales = df.groupby('Shipping Type')['Product Type'].count()
-
-    fig, ax = plt.subplots(figsize=(10, 5)) http://localhost:8501/
-    ax.bar(monthly_sales.index, monthly_sales.values, color='b') 
-    ax.set_title('Total Sales by Shipping Type')
-    ax.set_xlabel('Shipping Type')
-    ax.set_ylabel('Total Sales (Count)')
+    # Title + labels
+    plt.title('Shipping Type by Product Type')
+    plt.xlabel('Product Type')
+    plt.ylabel('Count')
     plt.xticks(rotation=45)
-
-    st.pyplot(fig)
-
-line_chart()
-
-st.write("### Key Observations:")
-st.write("- The chart highlights a strong preference for Standard shipping which dwarfs in comparison to other shipping types like Expedited, Express, and Overnight, while Same Day shipping shows the lowest sales count.")
-
-# Table 6 - Order Status by Product Type - Lim, K.
-def pie_chart():
-    df['Product Type'] = df['Product Type'].replace({
-        'smartphone': 'Smartphone',
-        'Smartwatch': 'Smartwatch',
-        'smartwatch': 'Smartwatch',
-        'Tablet': 'Tablet',
-        'tablet': 'Tablet',
-        'Laptop': 'Laptop',
-        'laptop': 'Laptop'
-    })
-
-    product_counts = df['Product Type'].value_counts()
-
-    fig, ax = plt.subplots(figsize=(8, 8))
-    pie_colors = ['skyblue', 'salmon', 'yellow', 'lightgreen', 'orange']
-    ax.pie(product_counts, labels=product_counts.index, autopct='%1.1f%%', startangle=140, colors=pie_colors)
+    plt.legend(title='Shipping Type')
+    st.pyplot(plt)
+    plt.clf()
+    st.write("""
+              The bar chart represents the shipping preference for different types of products: Headphones, Laptops, Smartphones, Smartwatches, and Tablets. 
+              As the chart is a stacked bar chart, each bar will show the proportion of use of shipping methods for any given category of product. 
+              This will easily compare the shipping preferences across the various products-understand customer behaviors and tendencies when purchasing these electronics.
+              \n\n
+              Observations:
+              *   Standard Shipping (orange) leads in most product categories, highly dominant in Smartphones.
+              *   Same Day Shipping (red) proves to be more popular for Laptops and Smartphones. This means that customers want their products delivered sooner, indicating, maybe, they want to receive their devices as quickly as they can.
+              *   Most categories have an almost equal balance of Overnight Shipping (green) and Express Shipping (pink), though both these modes are taken up less than Standard and Same Day.
+             """)
+#Tan Gabriel
+def order_status_by_prod_type():
+    """
+    bar chart showing the frequency of order status
+    (Completed vs Canceled) by product type.
+    """
+    # filters if status order is either completed or cancelled via boolean masking
+    orderstat = df['Order Status'].isin(['Completed', 'Cancelled'])
+    # creas a df containing only the relevant orders
+    filter = df[orderstat]
+    #creates a df to count the number of orders by Product Type and Order Status
+    order_fre = filter.groupby(['Product Type', 'Order Status']).size().unstack(fill_value=0)
+    # colors :D
+    colors = ['red', 'skyblue']
+    # creating a bar chart from the new df order_fre
+    order_fre.plot(kind='bar', color=colors, edgecolor='black', figsize=(10, 6))
+    plt.xlabel('Product Type')
+    plt.ylabel('Frequency')
+    plt.title('Order Status by Product Type')
+    plt.xticks(rotation=30)
+    plt.legend(title='Order Status')
     
-    ax.set_title('Order Status by Product Type')
-    ax.axis('equal')
+    st.pyplot(plt)
+    plt.clf()
+    st.write("""
+             For all products, the number of 'Completed' purchases significantly outweighs 
+             the number of cancelled purchases, indicating a strong satifaction/interest with 
+             the various products offered. Most pruchased product being 'SmartPhones' seconded 
+             by 'Tablet'. Meanwhile the most 'Cancelled' is 'Smartphones' seconded by 'Tablet' once again.
+             """)
 
-    st.pyplot(fig)
-
-pie_chart()
-
-st.write("### Key Observations:")
-st.write("- This graph highlights that customers prefer smartphones, leading at 29.9%, followed by tablets at 20.5%, laptops and smartwatches at 19.9% and 19.7%, and finally, headphones being the least preferred at 10.1%.")
-st.write("- The data showcases a high customer preference for handheld devices such as smartphones and tablets, with their accesssories, like smartwatches and headphones are less prefered.")
-
-
-# Table 7 - Payment Method Distribution - Ongtangco
 def pie_chart():
-    """
-    Payment Methods Distribution in a pie chart format
-    """
-    df['Payment Method'] = df['Payment Method'].replace({'Paypal': 'PayPal'})
+  """
+  Payment Methods Distribution in a pie chart format
+  """
+  df['Payment Method'] = df['Payment Method'].replace({'Paypal': 'PayPal'})
 
-    payment_counts = df['Payment Method'].value_counts()
-    labels = payment_counts.index
-    pie_colors = ['skyblue', 'salmon', 'yellow', 'lightgreen', 'orange']
+  payment_counts = df['Payment Method'].value_counts()
+  labels = payment_counts.index
+  pie_colors = ['skyblue', 'salmon', 'yellow', 'lightgreen', 'orange']
 
-    fig, ax = plt.subplots(figsize=(8, 8))
-    ax.pie(payment_counts, labels=labels, autopct='%1.1f%%', startangle=140, colors=pie_colors)
-    ax.set_title('Payment Method Distribution')
-    ax.axis('equal')
+  fig, ax = plt.subplots(figsize=(8, 8))
+  ax.pie(payment_counts, labels=labels, autopct='%1.1f%%', startangle=140, colors=pie_colors)
+  ax.set_title('Payment Method Distribution')
+  ax.axis('equal')
 
-    # Display the chart
-    st.pyplot(fig)
-
-st.header('Payment Method Distribution')
-pie_chart()
-st.write("The majority of customers utilized credit cards (29.3%) and PayPal (29.0%) for payments, followed by bank transfers (19.9%), cash (12.5%), and debit cards (12.4%).")
-
-# Table 8 - Total Sales Overtime - Ongtangco
+  # Display the chart
+  st.header('Payment Method Distribution')
+  st.pyplot(fig)
+  st.write("""
+            The majority of customers utilized credit cards (29.3%) and PayPal (29.0%) for payments, 
+            followed by bank transfers (19.9%), cash (12.5%), and debit cards (12.4%).
+            """)
 def line_chart():
     """
     Total sales over time in a line chart format
@@ -245,101 +232,82 @@ def line_chart():
     ax.set_xlabel('Purchase Date (in months)')
     ax.set_ylabel('Total Price (in millions)')
     plt.xticks(rotation=45)
-
     # Display the chart
     st.pyplot(fig)
-
-st.header('Total Sales Over Time')
-line_chart()
-st.write("Sales experienced a decline at the end of September, followed by a significant surge in December. This was followed by a decrease toward the end of December, with a gradual recovery beginning in late January, before sharply declining again at the start of September.")
-
-# Table 9 - Average Rating Over Time - Tan
-def average_rating_over_time():
+    st.write("""
+             Sales experienced a decline at the end of September, 
+             followed by a significant surge in December. This was followed 
+             by a decrease toward the end of December, with a gradual recovery 
+             beginning in late January, before sharply declining again at the 
+             start of September.
+             """)
+  #Ongtangco, Randolph Joshua
+def average_rating_over_time_linechart():
     """
-    Creates a line chart showing the average rating over time.
+        line chart
+        shows the average rating of products over time
     """
-    # Convert 'Purchase Date' to datetime
+
+    # 'Purchase Date' column -> datetime object
     df['Purchase Date'] = pd.to_datetime(df['Purchase Date'])
-    
-    # Group by month and calculate average rating
-    average_rating = df.groupby(df['Purchase Date'].dt.to_period('M'))['Rating'].mean()
 
-    # Create a line chart 
-    fig, ax = plt.subplots(figsize=(10, 5))
-    ax.set_facecolor('#0e1117')
-    fig.patch.set_facecolor('#0e1117')
+    # Purchase Date >> grouped
+    # Mean rating per day >> calculated
+    average_rating_by_month = df.groupby(df['Purchase Date'].dt.to_period('M'))['Rating'].mean()
+    plt.figure(figsize=(25, 4))
+    plt.plot(average_rating_by_month.index.astype(str), average_rating_by_month, marker='.', color='red')
 
-    # Plot the average rating data
-    ax.plot(average_rating.index.astype(str), average_rating.values, color='#83c9ff', linewidth=2, marker='o', label='Average Rating')
-
-    ax.set_title('Average Rating Over Time', color='white')
-    ax.set_xlabel('Purchase Month', color='white')
-    ax.set_ylabel('Average Rating', color='white')
-
-    ax.tick_params(axis='both', colors='white')
-
-    # Legend
-    legend = ax.legend(facecolor='#0e1117', edgecolor='white', fontsize='medium', loc='upper left', framealpha=0.7)
-    for text in legend.get_texts():
-        text.set_color('white')  
-
+    # Title + labels
+    plt.title('Monthly Average Rating')
+    plt.xlabel('Purchase Date')
+    plt.ylabel('Average Rating')
     plt.xticks(rotation=45)
-    st.pyplot(fig)
+    plt.grid(True)
+    st.pyplot(plt)
+    plt.clf()
+    st.write("""
+              The following graph represents the average rating given by the customers from September 2023 to September 2024. 
+              This graph is intended to demonstrate the trend of customer satisfaction on a 
+              monthly basis. It could possibly be useful in determining periods when customer satisfaction improved or worsened 
+              and might point to possible quality problems or improvement initiated by the company.
+              \n\n
+              **Observations:**
 
-    # Key Observations
-    st.write("### Key Observations:")
-    st.write("- The average rating showed a steady increase from September 2023 to December 2023..")
-    st.write("-  From January 2024 to March 2024, the average rating stabilized, indicating a stable customer perception of the products.")
-    st.write("-  Starting from April 2024, there were fluctuations in the observed average rating")
-    st.write("- However, a decline in average ratings was observed from June 2024 onwards.")
-
-st.title("Average Rating Over Time Period")
-average_rating_over_time()
-
-
-# Table 10 - Loyalty by Time - Tan
+              * There is a spiking in ratings in October 2023. Rating dips slightly in November 2023 but peaks during December 2023.
+              * Ratings then drop sharply in January 2024-one of the lowest ratings. Could be post-holiday decline or service/product issue during those times.
+              * There is a slight recovery of the ratings during February 2024 to September 2024, but has never reach the heights as rating during late 2023.
+            """)
+    #Tan Gabriel
 def loyalty_by_time_linechart():
     """
-    Creates a line chart showing the number of loyalty members over time.
+    creates a line char showing the number of purchases made by
+    loyalty members vs non-members over  a period of time.
     """
-    # Convert 'Purchase Date' string column to datetime object for easier handling
+    # converts 'Purchase Date' string collumn to datetime object for easier
     df['Purchase Date'] = pd.to_datetime(df['Purchase Date'])
 
-    # Group by month and count the number of unique Loyalty Members
-    loyalty_count = df.groupby(df['Purchase Date'].dt.to_period('M'))['Loyalty Member'].apply(lambda x: (x == 'Yes').sum())
-
-    # Create a line chart
-    fig, ax = plt.subplots(figsize=(10, 10))
-    ax.set_facecolor('#0e1117')
-    fig.patch.set_facecolor('#0e1117')
-
-    # Plot the data
-    ax.plot(loyalty_count.index.astype(str), loyalty_count.values, marker='o',color='#83c9ff', label='Loyalty Members')
-
-    ax.set_title('Loyalty Members Over Time', color='white')
-    ax.set_xlabel('Month', color='white')
-    ax.set_ylabel('Number of Loyalty Members', color='white')
-    ax.tick_params(axis='both', colors='white')
-
-    # Legend
-    legend = ax.legend(facecolor='#0e1117', edgecolor='white', fontsize='medium', loc='upper left', framealpha=0.7)
-    for text in legend.get_texts():
-        text.set_color('white') 
-
-    plt.xticks(rotation=45)
-    st.pyplot(fig)
-
-    # Add Key Observations below the graph
-    st.write("### Key Observations:")
-    st.write("- The number of loyalty members increased during September 2023 to February 2024, signaling a successful recruitment strategy during this period of time.")
-    st.write("- However from March 2024 to May 2024, the number of loyalty members remained relatively stable, meaning the growth rate stabalied during this time.")
-    st.write("- And finally the number of loyalty members decreased from June 2024 to September 2024, indicating a potential issue with member retention or a decline in new memberships.")
-
-
-st.title("Loyalty Members Over Time Period")
-loyalty_by_time_linechart()
-# Conclusion
-st.write("""
+    # groupby Purchase Date and  Member status, then count frequency
+    p_by_loyalty = df.groupby([df['Purchase Date'].dt.date, 'Loyalty Member']).size().unstack(fill_value=0)
+    plt.figure(figsize=(20, 6))
+    plt.plot(p_by_loyalty.index, p_by_loyalty['Yes'], marker='', label='Loyalty Member', color='blue')
+    plt.plot(p_by_loyalty.index, p_by_loyalty['No'], marker='', label='Non-Member', color='red')
+    plt.title('Purchases by Loyalty Members Over Time Period')
+    plt.xlabel('Date of Purchase')
+    plt.ylabel('Number of Purchases')
+    plt.xticks(rotation=30)
+    plt.legend(title='Loyalty Member')
+    plt.grid()
+    plt.tight_layout()
+    st.pyplot(plt)
+    plt.clf()
+    st.write("""
+            'Loyalty Member's tend to purchase less than 'Non-Member's with more 
+             consistency throuhout the year.Whereas 'Non-Member's tend to purchase 
+             more and with more  fluctuation through the year shown in the sharp increase at 
+             2024-01.
+             """)
+def conclusion():
+    st.write("""
         # Conclusion
         ### Customer Demographics
         * Customers are male and female, and are equally distributed.
@@ -373,3 +341,26 @@ st.write("""
         * Customer ratings have been average since they float around the 3 to 3.2 mark.
         * There are big fluctuations around the months of late 2023 to early 2024. The rating since then have been going slightly upward indicating that the company could be making decisions that improve their products or services. The big dip after December 2023, suggest that there are some inconsistencies either with the products or the services the company provides.
         """)
+############################################################################################
+# Graph 1 - Gender Distribution - Butial
+consumer_gender_distribution()
+# Graph 2 - Age Distribution - Butial
+age_distribution()
+# Graph 3 - Quantity Sold by Product Type - Lim, E.
+quantity_sold_by_prodtype()
+# Graph 4 - Quantity Sold by Smartphone Model - Lim, E.
+quantity_sold_by_smartphone_model()
+# Graph 5 - Shipping Type by Product Type - Lim, K.    
+shipping_type_by_product_bar()
+# Graph 6 - Order Status by Product Type - Lim, K.
+order_status_by_prod_type()
+# Graph 7 - Payment Method Distribution - Ongtangco
+pie_chart()
+# Graph 8 - Total Sales Overtime - Ongtangco
+line_chart()
+# Graph 9 - Average Rating Over Time - Tan
+average_rating_over_time_linechart()
+# Graph 10 - Loyalty by Time - Tan
+loyalty_by_time_linechart()
+# Conclusion
+conclusion()
